24e3df3355d29619b9233d63f872280e1d575d7f
Egor <KrylovWork@yandex.ru>
2022-08-25 09:49:23 +0500

    initial commit

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] array = new int[5];
+int positiveCount = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i}-е число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) positiveCount++;
+}
+//Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Положительных чисел в массиве: {positiveCount}.");
---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффициент при х уравнения первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный член уравнения первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент при х уравнения второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный член уравнения второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2- b1)/(k1- k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Искомая точка: ({x}, {y})");
